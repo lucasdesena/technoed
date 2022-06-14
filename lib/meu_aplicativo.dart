fa6703b83108898e1c2b3be3939e9577048ac16e
Lucas de Sena Bastos <lucasdesena@ucl.br>
2022-06-14 12:27:21 -0300

    Adicionado nova dependencia para trocar idioma do clipboard

--- lib/meu_aplicativo.dart
+++ lib/meu_aplicativo.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:technoed/widgets/auth_check.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MeuAplicativo extends StatelessWidget {
   const MeuAplicativo({Key? key}) : super(key: key);
@@ -7,21 +9,30 @@ class MeuAplicativo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'technoed',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color.fromARGB(255, 0, 180, 216),
-          ),
+      title: 'technoed',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromARGB(255, 0, 180, 216),
         ),
-        home: AuthCheck(),
-        onGenerateRoute: (RouteSettings settings) {
-          switch (settings.name) {
-            case '/authCheck':
-              return MaterialPageRoute(builder: (context) => AuthCheck());
-            default:
-              return null;
-          }
-        });
+      ),
+      home: AuthCheck(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/authCheck':
+            return MaterialPageRoute(builder: (context) => AuthCheck());
+          default:
+            return null;
+        }
+      },
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('pt', 'BR'),
+      ],
+    );
   }
 }
